134272772508a8f09c6a850ac4d43c7a18d64426
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-06-18 14:35:37 +0200

    Create basic working REST-API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[post("/api/resource")]
+async fn create_resource(_payload: web::Json<CreateResourceRequest>) -> impl Responder {
+    // Handle the POST request and return an appropriate response
+    HttpResponse::Ok().body("Resource created successfully")
+}
+
+#[derive(serde::Deserialize)]
+struct CreateResourceRequest {
+    // Define the request payload structure here
+    // Example: `name: String, age: u32, etc.`
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(create_resource)
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
